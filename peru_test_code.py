c039a57ff69c977695c697535e311bde5ddbe407
skarakas1 <skarakas21@ucla.edu>
2022-07-16 20:43:15 -0700

    this 'test_code' script cleans input significantly

--- peru_test_code.py
+++ peru_test_code.py
@@ -1,6 +1,7 @@
 # INPUT
 # Define the input excel
 infile = "Peru_pdf_clip.xlsx"
+outfile = "Peru_test_output.xlsx"
 
 # Important packages and functions
 import sys, datetime, xlsxwriter, openpyxl
@@ -11,27 +12,46 @@ print('Script starts at: ' + str(datetime.datetime.now()))
 infile_obj = openpyxl.load_workbook(infile) #see more: https://openpyxl.readthedocs.io/en/stable/
 infile_sheet = infile_obj.active #active spreadsheet of this excel
 
+# Create an empty output excel spreadsheet
+outfile_obj = xlsxwriter.Workbook(outfile) #see more: https://xlsxwriter.readthedocs.io/
+outfile_sheet = outfile_obj.add_worksheet("dams_subset") #any name you want to give to the output spreadsheet
+
 # Loop through each record in selected range
 row_number = 0 #initiate an integer to indicate the row index in the input spreadsheet.
-
-#printing cells A1 through BP8 to see what kind of values python is reading from this heavily formatted spreadsheet
+row_number_write = 0 #initiate an integer to indicate the row index in the output spreadsheet. 
 
 def print_row_num():
     print('looping row#: ' + str(row_number))
-#define cell range
-test_range = infile_sheet['A1':'BP8']
+
 #for loop to read infile
-for each_row in test_range:
+for each_row in infile_sheet:
     
+    #create cell # variable w/in row loop to start cell numbering at 0 for each row
     cell_number = 0
-    
+
     print_row_num()
 
-    for each_cell in each_row:
-        if each_cell.value:
-            print('row' + str(row_number) + ' cell: ' + str(cell_number) + ' = ' + str(each_cell.value))
+    row = []
 
-            cell_number = cell_number + 1
-            #count which cell we are reading
+    #loop to iterate through cells
+    for cell in each_row:
+        
+        #skip empty cells
+        if cell.value:
+
+            row.append(cell.value)
+
+            outfile_sheet.write_row(row_number_write, 0, row)
+            #row_number_write = row_number_write + 1
+
+            #output string
+            #print('row ' + str(row_number) + ' cell: ' + str(cell_number) + ' = ' + str(cell.value))
+
+            #cell_number = cell_number + 1 #count which cell we are reading
 
     row_number = row_number + 1 #count which row we are reading from the input spreadsheet.
+    row_number_write = row_number_write + 1
+
+outfile_obj.close() #close output file
+
+print('Script ends at: ' + str(datetime.datetime.now())) #print end time
